c4f67570745470bf6b02df67149cfebdf352a26d
KatyaMy <20FamGam20@gmail.com>
2021-11-05 19:20:05 -0400

    Metod lesson10

---
+++ MetodSubstring.js
@@ -0,0 +1,5 @@
+let str = 'Helloworld';
+console.log(str.length);
+console.log(str.substring(0, 9)); // "Hellow"
+console.log(str.substring(3, 6)); // "low"
+console.log(str.substring(6, 6));
---
+++ TaskLesson10Exmp.js
@@ -0,0 +1,14 @@
+let str = 'I';
+let str2 = 'am';
+let str3 = ' a student'
+
+ let res = str.concat(' ', str2, str3);
+console.log(res);
+
+//ex2.
+
+let hello = 'Hello, ';
+let name = 'Julia';
+ console.log(hello.concat(`${name}`, '. Have a nice day.'))
+
+//Hello, Kevin. Have a nice day.
